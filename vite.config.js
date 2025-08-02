ed2d2d57e7fd0aed8aab876fb6a9d9f7dbef4f8a
Kishor-Irnak <kishorirnak4u@gmail.com>
2025-08-02 23:00:57 +0530

    fixing path bug

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/<repository-name>/",  // <-- important
+  base: "/nexus-react-app/",  // <-- add leading and trailing slash
   plugins: [react()],
 });
